88f388c386e98444f7a618e53b41708042ced2b8
Amoorer75 <Amoorer127@gmail.com>
2022-02-27 19:27:37 -0500

    first init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import * as actionCreator from "./store/actions/actions";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Age-label">
+          your age: <span>{this.props.age}</span>
+        </div>
+        <button onClick={this.props.onAgeUp}>Age UP</button>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
+        {this.props.loading && <img src={logo} className="App-logo" />}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    age: state.age,
+    loading: state.loading
+  };
+};
+
+const mapDispachToProps = dispatch => {
+  return {
+    onAgeUp: () => dispatch(actionCreator.ageUp(1)),
+    onAgeDown: () => dispatch(actionCreator.ageDown(1))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispachToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reducer from "./store/reducers/reducer";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
